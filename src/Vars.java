01ddb9400cab082781f4fce5ccacfe15f43166c9
usuario <usuario@Dev-VirtualBox>
2019-11-05 11:26:52 +0100

    Final Edition

---
+++ src/Vars.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author usuario
+ */
+public class Vars {
+
+    private String name;
+    private String value;
+
+    public Vars(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }  
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+    
+
+}
